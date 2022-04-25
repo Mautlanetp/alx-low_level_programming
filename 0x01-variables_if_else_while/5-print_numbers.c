8b5bb4451854edc3241c7ba43571d2516715559d
Mautlanetp <tshegofatsopietmautlane@gmail.com>
2022-04-25 21:13:05 +0100

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints single digit numbers
+ *
+ * Return: Always 0(seccess)
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
