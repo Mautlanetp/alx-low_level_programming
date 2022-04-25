7c39bae4bea2e1eebdece4214408015fdff42032
Mautlanetp <tshegofatsopietmautlane@gmail.com>
2022-04-25 21:41:54 +0100

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - prints all single digits numbers of the base of 10
+ *
+ * Return: Always 0 (seccess)
+ */
+
+int main(void)
+{
+	
+	int i;
+	
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	
+
+	putchar('\n');
+
+	return (0);
+}
