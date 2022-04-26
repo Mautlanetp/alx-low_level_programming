d0a6f4c3b18e29b455bec89445f0185cfbe25670
Mautlanetp <tshegofatsopietmautlane@gmail.com>
2022-04-26 14:43:21 +0100

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - will print 10 time the alphabet in lowercase
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		/**
+		 * _putchar('\n');
+		 */
+
+		for (i = 97; i < 123; i++)
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
